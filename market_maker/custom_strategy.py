57b66bd5613583b18415416c04872f25e4102012
qinzej <>
2020-01-06 08:55:19 +0800

    提交新的策略：为了完成更多的交易

--- market_maker/custom_strategy.py
+++ market_maker/custom_strategy.py
@@ -2,24 +2,82 @@ import sys
 
 from market_maker.market_maker import OrderManager
 
+from market_maker.utils import log, constants, errors, math
+from market_maker.settings import settings
+
+
+#
+# Helpers
+#
+logger = log.setup_custom_logger('xxx')
+
 
 class CustomOrderManager(OrderManager):
     """A sample order manager for implementing your own custom strategy"""
+    def get_ticker(self):
+        ticker = self.exchange.get_ticker()
+        tickLog = self.exchange.get_instrument()['tickLog']
 
-    def place_orders(self) -> None:
-        # implement your custom strategy here
+        # Set up our buy & sell positions as the smallest possible unit above and below the current spread
+        # and we'll work out from there. That way we always have the best price but we don't kill wide
+        # and potentially profitable spreads.
+        self.start_position_buy = ticker["buy"] + self.instrument['tickSize']
+        self.start_position_sell = ticker["sell"]
 
-        buy_orders = []
-        sell_orders = []
+        # If we're maintaining spreads and we already have orders in place,
+        # make sure they're not ours. If they are, we need to adjust, otherwise we'll
+        # just work the orders inward until they collide.
+        if settings.MAINTAIN_SPREADS:
+            if ticker['buy'] == self.exchange.get_highest_buy()['price']:
+                self.start_position_buy = ticker["buy"]
+            if ticker['sell'] == self.exchange.get_lowest_sell()['price']:
+                self.start_position_sell = ticker["sell"]
 
-        # populate buy and sell orders, e.g.
-        # buy_orders.append({'price': 999.0, 'orderQty': 100, 'side': "Buy"})
-        # sell_orders.append({'price': 1001.0, 'orderQty': 100, 'side': "Sell"})
+        # Back off if our spread is too small.
+        if self.start_position_buy >= self.start_position_sell:
+            self.start_position_buy -= self.instrument['tickSize']
+            # self.start_position_sell += self.instrument['tickSize'] * settings.ORDER_PAIRS / 2
 
-        self.converge_orders(buy_orders, sell_orders)
+        # Midpoint, used for simpler order placement.
+        self.start_position_mid = ticker["mid"]
+        logger.info(
+            "%s Ticker: Buy: %.*f, Sell: %.*f, tickSize: %.*f" %
+            (self.instrument['symbol'], tickLog, ticker["buy"], tickLog, ticker["sell"],
+             tickLog, self.instrument['tickSize'])
+        )
+        logger.info('Start Positions: Buy: %.*f, Sell: %.*f, Mid: %.*f' %
+                    (tickLog, self.start_position_buy, tickLog, self.start_position_sell,
+                     tickLog, self.start_position_mid))
+        return ticker
+
+    def get_price_offset(self, index):
+        """Given an index (1, -1, 2, -2, etc.) return the price for that side of the book.
+           Negative is a buy, positive is a sell."""
+
+        tickSize = self.instrument['tickSize']
+
+        # Maintain existing spreads for max profit
+        if settings.MAINTAIN_SPREADS:
+            start_position = self.start_position_buy if index < 0 else self.start_position_sell
+            # First positions (index 1, -1) should start right at start_position, others should branch from there
+            index = index + 1 if index < 0 else index - 1
+        else:
+            # Offset mode: ticker comes from a reference exchange and we define an offset.
+            start_position = self.start_position_buy if index < 0 else self.start_position_sell
+
+            # If we're attempting to sell, but our sell price is actually lower than the buy,
+            # move over to the sell side.
+            if index > 0 and start_position < self.start_position_buy:
+                start_position = self.start_position_sell
+            # Same for buys.
+            if index < 0 and start_position > self.start_position_sell:
+                start_position = self.start_position_buy
+
+        return math.toNearest((start_position + tickSize * index), self.instrument['tickSize'])
 
 
-def run() -> None:
+def run():
+    logger.info('BitMEX Market custom strategy \n')
     order_manager = CustomOrderManager()
 
     # Try/except just keeps ctrl-c from printing an ugly stacktrace
